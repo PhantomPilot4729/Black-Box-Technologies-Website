762d4c2f6e18cc99058779050424f0deb7ec6be6
FutureAlpha4729 <mmh858sd@gmail.com>
2026-02-26 12:10:43 -0800

    Add SessionWrapper client component

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import { SessionProvider } from "next-auth/react";
+import SessionWrapper from "../components/SessionWrapper";
 
 export const metadata: Metadata = {
   title: "Black Box Technologies",
@@ -17,11 +18,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <SessionProvider>
+        <SessionWrapper>
           <Navbar />
           {children}
           <Footer />
-        </SessionProvider>
+        </SessionWrapper>
       </body>
     </html>
   );
